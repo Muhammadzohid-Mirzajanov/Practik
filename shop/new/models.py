a95dbb8a6b19d4d2470bc3f193df6deec5e17cda
Vizarus <vizrus001>
2024-12-09 18:28:55 +0300

    +

--- shop/new/models.py
+++ shop/new/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100,verbose_name="Kategoriya ismi")
+    photo = models.ImageField(upload_to='photos/',verbose_name='categoriya rasmi')
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    created_at = models.DateTimeField()
+    updated_at = models.TimeField()
+    photo = models.ImageField(upload_to='photos/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+
--- shop/new/views.py
+++ shop/new/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def category_list(request):
+    categorys = Category.objects.all()
+    return render(request, "index.html", context={"category": categorys})
+
